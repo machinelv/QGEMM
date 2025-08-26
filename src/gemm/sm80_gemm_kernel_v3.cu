39dd335be97c209c5e8c85d741ff4b8a6f89ec01
machinelvqz <machinelvqz@outlook.com>
2025-08-27 00:00:23 +0800

    [GEMM] feat: add padding method on sm80 v3

--- src/gemm/sm80_gemm_kernel_v3.cu
+++ src/gemm/sm80_gemm_kernel_v3.cu
@@ -18,106 +18,13 @@ using namespace gemm_kernel;
 using WMMA_BF16_Config = gemm_kernel::sm80::WMMA_BF16_Config;
 
 namespace {
-template <  size_t BLOCK_TILE_SIZE_M, size_t BLOCK_TILE_SIZE_N, size_t BLOCK_TILE_SIZE_K,
-            size_t WARP_TILE_SIZE_M, size_t WARP_TILE_SIZE_N, 
-            size_t WMMA_TILE_PER_WARP_M, size_t WMMA_TILE_PER_WARP_N, size_t WMMA_TILE_PER_WARP_K,
-            size_t WMMA_TILE_SIZE_M, size_t WMMA_TILE_SIZE_N, size_t WMMA_TILE_SIZE_K,
-            size_t WMMA_REG_PER_THREAD_A, size_t WMMA_REG_PER_THREAD_B, size_t WMMA_REG_PER_THREAD_C>
-inline __device__ void process_data_from_shared_memory_using_wmma_bf16_transposed(
-        const __nv_bfloat16* A_shared_block_tile,
-        const __nv_bfloat16* B_T_shared_block_tile,
-        uint32_t a_frag[WMMA_TILE_PER_WARP_M][WMMA_REG_PER_THREAD_A],
-        uint32_t b_frag[WMMA_TILE_PER_WARP_N][WMMA_REG_PER_THREAD_B],
-        uint32_t acc_frag_fp32[WMMA_TILE_PER_WARP_M][WMMA_TILE_PER_WARP_N][WMMA_REG_PER_THREAD_C],
-        size_t M, size_t N, size_t K,
-        size_t warp_m_id, size_t warp_n_id)
-{
-    static_assert(WMMA_REG_PER_THREAD_A == 4, "WMMA_REG_PER_THREAD_A must be 4");
-    static_assert(WMMA_REG_PER_THREAD_B == 4, "WMMA_REG_PER_THREAD_B must be 4");
-    static_assert(WMMA_REG_PER_THREAD_C == 8, "WMMA_REG_PER_THREAD_C must be 4");
 
-    size_t thread_id = threadIdx.x + threadIdx.y * blockDim.x;
-    size_t lane_id = thread_id % WARP_SIZE;
-
-    constexpr auto get_log_2 = [](size_t x) {
-        size_t i = 0;
-        while((x & 0x1) == 0) {
-            i ++;
-            x = x >> 1;
-        }
-        return i;
-    };
-    
-    constexpr size_t trunk_size_bits = 128;
-    constexpr size_t element_per_trunk = trunk_size_bits / 8 / sizeof(__nv_bfloat16);
-    constexpr size_t trunk_per_cacheline_k = BLOCK_TILE_SIZE_K / element_per_trunk;
-    // constexpr size_t trunk_per_cacheline_n = BLOCK_TILE_SIZE_N / element_per_trunk;
-
-    constexpr size_t trunk_per_warp_k = WMMA_TILE_SIZE_K / element_per_trunk;
-    constexpr size_t trunk_per_warp_m = WMMA_TILE_SIZE_M;
-    constexpr size_t trunk_per_warp_n = WMMA_TILE_SIZE_N;
-    // constexpr size_t trunk_per_warp_n = WMMA_TILE_SIZE_N;
-    
-    size_t trunk_row_idx_A = lane_id % trunk_per_warp_m;
-    size_t trunk_col_idx_A = lane_id / trunk_per_warp_m;
-
-    size_t trunk_row_idx_n = lane_id % trunk_per_warp_n;
-    size_t trunk_col_idx_n = lane_id / trunk_per_warp_n;
-
-    // process wmma tile
-    #pragma unroll(WMMA_TILE_PER_WARP_K)
-    for (size_t wmma_tile_idx_k{0U}; wmma_tile_idx_k < WMMA_TILE_PER_WARP_K; ++wmma_tile_idx_k) {
-        // Load data from shared memory to register
-        // size_t block_tile_wmma_tile_k_idx{wmma_tile_idx_k * WMMA_TILE_SIZE_K};
-        // Load A and B matrices from shared memory to registers
-        #pragma unroll(WMMA_TILE_PER_WARP_M)
-        for (size_t wmma_tile_idx_m{0U}; wmma_tile_idx_m < WMMA_TILE_PER_WARP_M; ++wmma_tile_idx_m) {
-            // A_shared_block_tile's row offset
-            size_t block_tile_wmma_tile_m_idx{warp_m_id * WARP_TILE_SIZE_M + wmma_tile_idx_m * WMMA_TILE_SIZE_M};
-            size_t smem_index = (trunk_col_idx_A + wmma_tile_idx_k * trunk_per_warp_k) * element_per_trunk + trunk_row_idx_A * BLOCK_TILE_SIZE_K;
-            shared_memory_swizzle_coordinate<get_log_2(trunk_per_cacheline_k), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_M)>(smem_index);
-            uint32_t smem_addr = __cvta_generic_to_shared(&A_shared_block_tile[block_tile_wmma_tile_m_idx * BLOCK_TILE_SIZE_K + smem_index]);
-            LDMATRIX_BF16_X4(a_frag[wmma_tile_idx_m][0], a_frag[wmma_tile_idx_m][1], a_frag[wmma_tile_idx_m][2], a_frag[wmma_tile_idx_m][3], smem_addr);
-        }
-
-        #pragma unroll(WMMA_TILE_PER_WARP_N)
-        for (size_t wmma_tile_idx_n{0U}; wmma_tile_idx_n < WMMA_TILE_PER_WARP_N; ++wmma_tile_idx_n) {
-            // B_shared_block_tile's column offset
-            size_t block_tile_wmma_tile_n_idx{warp_n_id * WARP_TILE_SIZE_N + wmma_tile_idx_n * WMMA_TILE_SIZE_N};
-            size_t smem_index = (trunk_row_idx_n * BLOCK_TILE_SIZE_K + (trunk_col_idx_n + wmma_tile_idx_k * trunk_per_warp_k) * element_per_trunk);
-            shared_memory_swizzle_coordinate<get_log_2(trunk_per_cacheline_k), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_N)>(smem_index);
-            // B_T_shared_block_tile[BLOCK_TILE_SIZE_N][BLOCK_TILE_SIZE_K]
-            uint32_t smem_addr = __cvta_generic_to_shared(&B_T_shared_block_tile[block_tile_wmma_tile_n_idx * BLOCK_TILE_SIZE_K + smem_index]);
-            // There is a transpose between 
-            LDMATRIX_BF16_X4(b_frag[wmma_tile_idx_n][0], b_frag[wmma_tile_idx_n][2], b_frag[wmma_tile_idx_n][1], b_frag[wmma_tile_idx_n][3], smem_addr);
-        }
-
-        // Compute the acc_frag_fp32
-        #pragma unroll(WMMA_TILE_PER_WARP_M)
-        for (size_t wmma_tile_idx_m{0U}; wmma_tile_idx_m < WMMA_TILE_PER_WARP_M; ++wmma_tile_idx_m) {
-            #pragma unroll(WMMA_TILE_PER_WARP_N)
-            for (size_t wmma_tile_idx_n{0U}; wmma_tile_idx_n < WMMA_TILE_PER_WARP_N; ++wmma_tile_idx_n) {
-                // wmma::mma_sync(acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n], a_frag[wmma_tile_idx_m], b_frag[wmma_tile_idx_n], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n]);
-                BF16F32MMA16816(acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][0], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][1], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][2], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][3],
-                a_frag[wmma_tile_idx_m][0], a_frag[wmma_tile_idx_m][1], a_frag[wmma_tile_idx_m][2], a_frag[wmma_tile_idx_m][3],
-                b_frag[wmma_tile_idx_n][0], b_frag[wmma_tile_idx_n][1],
-                acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][0], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][1], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][2], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][3]);
-
-                BF16F32MMA16816(acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][4], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][5], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][6], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][7],
-                a_frag[wmma_tile_idx_m][0], a_frag[wmma_tile_idx_m][1], a_frag[wmma_tile_idx_m][2], a_frag[wmma_tile_idx_m][3],
-                b_frag[wmma_tile_idx_n][2], b_frag[wmma_tile_idx_n][3],
-                acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][4], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][5], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][6], acc_frag_fp32[wmma_tile_idx_m][wmma_tile_idx_n][7]);
-            }
-        }
-    }
-    __syncthreads();
-}
 
 template <  size_t BLOCK_TILE_SIZE_M, size_t BLOCK_TILE_SIZE_N, size_t BLOCK_TILE_SIZE_K,
             size_t WARP_TILE_SIZE_M, size_t WARP_TILE_SIZE_N, 
             size_t WMMA_TILE_PER_WARP_M, size_t WMMA_TILE_PER_WARP_N, size_t WMMA_TILE_PER_WARP_K,
             size_t WMMA_TILE_SIZE_M, size_t WMMA_TILE_SIZE_N, size_t WMMA_TILE_SIZE_K,
-            size_t WMMA_REG_PER_THREAD_A, size_t WMMA_REG_PER_THREAD_B, size_t WMMA_REG_PER_THREAD_C>
+            size_t WMMA_REG_PER_THREAD_A, size_t WMMA_REG_PER_THREAD_B, size_t WMMA_REG_PER_THREAD_C, size_t PADDING = 0>
 inline __device__ void process_data_from_shared_memory_using_wmma_bf16(
         const __nv_bfloat16* A_shared_block_tile,
         const __nv_bfloat16* B_shared_block_tile,
@@ -145,8 +52,8 @@ inline __device__ void process_data_from_shared_memory_using_wmma_bf16(
     
     constexpr size_t trunk_size_bits = 128;
     constexpr size_t element_per_trunk = trunk_size_bits / 8 / sizeof(__nv_bfloat16);
-    constexpr size_t trunk_per_cacheline_k = BLOCK_TILE_SIZE_K / element_per_trunk;
-    constexpr size_t trunk_per_cacheline_n = BLOCK_TILE_SIZE_N / element_per_trunk;
+    constexpr size_t trunk_per_line_k = (BLOCK_TILE_SIZE_K + PADDING) / element_per_trunk;
+    constexpr size_t trunk_per_line_n = (BLOCK_TILE_SIZE_N + PADDING) / element_per_trunk;
 
     constexpr size_t trunk_per_warp_k = WMMA_TILE_SIZE_K / element_per_trunk;
     constexpr size_t trunk_per_warp_m = WMMA_TILE_SIZE_M;
@@ -173,9 +80,9 @@ inline __device__ void process_data_from_shared_memory_using_wmma_bf16(
             size_t m_idx = block_tile_wmma_tile_m_idx + trunk_row_idx_A ;
             size_t k_idx = (trunk_col_idx_A + wmma_tile_idx_k * trunk_per_warp_k);
             // k_idx ^= m_idx;
-            size_t smem_index = k_idx * element_per_trunk + m_idx * BLOCK_TILE_SIZE_K;
+            size_t smem_index = k_idx * element_per_trunk + m_idx * (BLOCK_TILE_SIZE_K + PADDING);
             // size_t smem_index = (trunk_col_idx_A + wmma_tile_idx_k * trunk_per_warp_k) * element_per_trunk + trunk_row_idx_A * BLOCK_TILE_SIZE_K;
-            // shared_memory_swizzle_coordinate<get_log_2(trunk_per_cacheline_k), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_M)>(smem_index);
+            // shared_memory_swizzle_coordinate<get_log_2(trunk_per_line_k), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_M)>(smem_index);
             uint32_t smem_addr = __cvta_generic_to_shared(&A_shared_block_tile[smem_index]);
             LDMATRIX_BF16_X4(a_frag[wmma_tile_idx_m][0], a_frag[wmma_tile_idx_m][1], a_frag[wmma_tile_idx_m][2], a_frag[wmma_tile_idx_m][3], smem_addr);
         }
@@ -187,8 +94,8 @@ inline __device__ void process_data_from_shared_memory_using_wmma_bf16(
             size_t n_idx = block_tile_wmma_tile_n_idx + (trunk_col_idx_B) * element_per_trunk;
             size_t k_idx = (trunk_row_idx_B + wmma_tile_idx_k * WMMA_TILE_SIZE_K);
             // k_idx ^= trunk_col_idx_B;
-            size_t smem_index = n_idx + k_idx * BLOCK_TILE_SIZE_N;
-            // shared_memory_swizzle_coordinate<get_log_2(trunk_per_cacheline_n), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_N)>(smem_index);
+            size_t smem_index = n_idx + k_idx * (BLOCK_TILE_SIZE_N + PADDING);
+            // shared_memory_swizzle_coordinate<get_log_2(trunk_per_line_n), get_log_2(trunk_size_bits), get_log_2(WMMA_TILE_SIZE_N)>(smem_index);
             uint32_t smem_addr = __cvta_generic_to_shared(&B_shared_block_tile[smem_index]);
             // The ldmatrix will transpose it self, there is no need to transpose
             LDMATRIX_BF16_TRANSPOSE_X4(b_frag[wmma_tile_idx_n][0], b_frag[wmma_tile_idx_n][1], b_frag[wmma_tile_idx_n][2], b_frag[wmma_tile_idx_n][3], smem_addr);
@@ -222,7 +129,7 @@ namespace sm80{
 template <typename T_OUTPUT, size_t BLOCK_TILE_SIZE_M, size_t BLOCK_TILE_SIZE_N, size_t BLOCK_TILE_SIZE_K,
           size_t WARP_TILE_SIZE_M, size_t WARP_TILE_SIZE_N, 
           size_t WMMA_TILE_SIZE_M, size_t WMMA_TILE_SIZE_N, size_t WMMA_TILE_SIZE_K,
-          size_t GROUP_SIZE_M>
+          size_t GROUP_SIZE_M, size_t PADDING = 0>
 __global__ void GEMM_kernel_bf16_v3(const __nv_bfloat16* A, size_t ldA, const __nv_bfloat16* B, size_t ldB,
                        T_OUTPUT* C, size_t ldC, size_t M, size_t N, size_t K)
 {
@@ -282,8 +189,7 @@ __global__ void GEMM_kernel_bf16_v3(const __nv_bfloat16* A, size_t ldA, const __
 
     extern __shared__ __nv_bfloat16 shared_memory[];
     __nv_bfloat16* A_block_tile = &shared_memory[0];
-    __nv_bfloat16* B_block_tile = &shared_memory[BLOCK_TILE_SIZE_K * BLOCK_TILE_SIZE_M];
-    __nv_bfloat16* B_T_block_tile = &shared_memory[BLOCK_TILE_SIZE_K * BLOCK_TILE_SIZE_M];
+    __nv_bfloat16* B_block_tile = &shared_memory[(BLOCK_TILE_SIZE_K + PADDING) * BLOCK_TILE_SIZE_M];
     // T_OUTPUT* C_block_tile = reinterpret_cast<T_OUTPUT*>(&shared_memory[0]);
 
     // wmma::fragment<wmma::accumulator, WMMA_TILE_SIZE_M, WMMA_TILE_SIZE_N, WMMA_TILE_SIZE_K, float> acc_frag_fp32[WMMA_TILE_PER_WARP_M][WMMA_TILE_PER_WARP_N];
@@ -313,8 +219,8 @@ __global__ void GEMM_kernel_bf16_v3(const __nv_bfloat16* A, size_t ldA, const __
         size_t block_tile_start_k = block_id_k;
         // load A and B matrices from global memory to shared memory
         load_data_from_global_memory_to_shared_memory_async
-            <__nv_bfloat16, BLOCK_TILE_SIZE_M, BLOCK_TILE_SIZE_N, BLOCK_TILE_SIZE_K, THREAD_NUM>
-            (A, B, A_block_tile, B_T_block_tile, M, N, K,
+            <__nv_bfloat16, BLOCK_TILE_SIZE_M, BLOCK_TILE_SIZE_N, BLOCK_TILE_SIZE_K, THREAD_NUM, float4, PADDING>
+            (A, B, A_block_tile, B_block_tile, M, N, K,
              block_tile_start_m, block_tile_start_n, block_tile_start_k, thread_id);
         cp_async_wait_all;
         __syncthreads();
@@ -325,8 +231,8 @@ __global__ void GEMM_kernel_bf16_v3(const __nv_bfloat16* A, size_t ldA, const __
             WARP_TILE_SIZE_M, WARP_TILE_SIZE_N, 
             WMMA_TILE_PER_WARP_M, WMMA_TILE_PER_WARP_N, WMMA_TILE_PER_WARP_K,
             WMMA_TILE_SIZE_M, WMMA_TILE_SIZE_N, WMMA_TILE_SIZE_K, 
-            WMMA_REG_PER_THREAD_A, WMMA_REG_PER_THREAD_B, WMMA_REG_PER_THREAD_C>
-            (A_block_tile, B_T_block_tile, a_frag, b_frag, acc_frag_fp32, M, N, K, warp_m_id, warp_n_id);
+            WMMA_REG_PER_THREAD_A, WMMA_REG_PER_THREAD_B, WMMA_REG_PER_THREAD_C, PADDING>
+            (A_block_tile, B_block_tile, a_frag, b_frag, acc_frag_fp32, M, N, K, warp_m_id, warp_n_id);
         __syncthreads();
     }
 
@@ -376,13 +282,13 @@ void launch_bf16_kernel_v3(const __nv_bfloat16* A, size_t ldA, const __nv_bfloat
     constexpr size_t WARP_NUM_N{BLOCK_TILE_SIZE_N / WMMA_BF16_Config::WARP_TILE_SIZE_N};
     constexpr size_t WARP_NUM{WARP_NUM_M * WARP_NUM_N};
     constexpr size_t THREAD_NUM{WARP_NUM * WARP_SIZE};
-
-    constexpr size_t SHARED_MEM_SIZE = BLOCK_TILE_SIZE_K*(BLOCK_TILE_SIZE_N + BLOCK_TILE_SIZE_M) * sizeof(__nv_bfloat16);
+    constexpr size_t PADDING = 8;
+    constexpr size_t SHARED_MEM_SIZE = (BLOCK_TILE_SIZE_K * (BLOCK_TILE_SIZE_N + PADDING) + (BLOCK_TILE_SIZE_K + PADDING) * BLOCK_TILE_SIZE_M) * sizeof(__nv_bfloat16);
 
     GEMM_kernel_bf16_v3<float, BLOCK_TILE_SIZE_M, BLOCK_TILE_SIZE_N, BLOCK_TILE_SIZE_K, 
                     WMMA_BF16_Config::WARP_TILE_SIZE_M, WMMA_BF16_Config::WARP_TILE_SIZE_N, 
                     WMMA_BF16_Config::WMMA_TILE_SIZE_M, WMMA_BF16_Config::WMMA_TILE_SIZE_N, WMMA_BF16_Config::WMMA_TILE_SIZE_K, 
-                    GROUP_SIZE_M>
+                    GROUP_SIZE_M, PADDING>
         <<<dim3((N + BLOCK_TILE_SIZE_N - 1) / BLOCK_TILE_SIZE_N, (M + BLOCK_TILE_SIZE_M - 1) / BLOCK_TILE_SIZE_M, 1),
            dim3(THREAD_NUM), SHARED_MEM_SIZE>>>(A, ldA, B, ldB, C, ldC, M, N, K);
     cudaError_t err = cudaGetLastError();
